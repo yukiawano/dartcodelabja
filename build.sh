be260141092041bcc1f6ca3a82c3207a33befce7
Yuki Awano <y.a.unix@gmail.com>
2012-08-11 17:17:59 +0900

    MINOR Added step4

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-cat header.tex introduction/introduction.tex step1/step1.tex step2/step2.tex step3/step3.tex footer.tex > cat.tex
+cat header.tex introduction/introduction.tex step1/step1.tex step2/step2.tex step3/step3.tex step4/step4.tex footer.tex > cat.tex
 platex cat.tex
 dvipdfmx cat.dvi
