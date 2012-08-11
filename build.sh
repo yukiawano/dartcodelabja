f9bc62fda1313cd0f28fba3635516d950eee77f0
Yuki Awano <y.a.unix@gmail.com>
2012-08-11 19:00:04 +0900

    MINOR Changed build.sh to run ebb for all directories.

--- build.sh
+++ build.sh
@@ -1,3 +1,13 @@
+ebb introduction/*.png
+ebb step1/*.png
+ebb step2/*.png
+ebb step3/*.png
+ebb step4/*.png
+ebb step5/*.png
+ebb step6/*.png
+ebb step7/*.png
+ebb step8/*.png
+ebb step9/*.png
 cat header.tex introduction/introduction.tex step1/step1.tex step2/step2.tex step3/step3.tex step4/step4.tex step5/step5.tex step6/step6.tex step7/step7.tex step8/step8.tex step9/step9.tex footer.tex > cat.tex
 platex cat.tex
 dvipdfmx cat.dvi
--- step1/sendfeedback2.bb
+++ step1/sendfeedback2.bb
@@ -1,5 +1,5 @@
-%%Title: ./step1/welcome.png
+%%Title: ./step1/sendfeedback2.png
 %%Creator: extractbb 20090708
-%%BoundingBox: 0 0 243 135
+%%BoundingBox: 0 0 356 240
 %%CreationDate: Sat Aug 11 18:59:09 2012
 
