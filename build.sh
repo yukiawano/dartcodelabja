edd6724c11dd93411f780398ca2746acaf57d6ca
Yuki Awano <y.a.unix@gmail.com>
2012-08-11 18:30:15 +0900

    MINOR Changed build.sh not to repeat platex. Because running platex multiple times makes debugging difficult.

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 cat header.tex introduction/introduction.tex step1/step1.tex step2/step2.tex step3/step3.tex step4/step4.tex step5/step5.tex step6/step6.tex step7/step7.tex step8/step8.tex step9/step9.tex footer.tex > cat.tex
 platex cat.tex
-platex cat.tex
 dvipdfmx cat.dvi
